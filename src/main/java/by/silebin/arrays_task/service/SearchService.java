2a3b82383fea0b107893716492690c51a68dacf3
prplcld <antonsilebin@gmail.com>
2021-03-25 09:57:02 +0300

    added more constructors to exception

--- src/main/java/by/silebin/arrays_task/service/SearchService.java
+++ src/main/java/by/silebin/arrays_task/service/SearchService.java
@@ -4,6 +4,6 @@ import by.silebin.arrays_task.entity.ArrayEntity;
 import by.silebin.arrays_task.service.exception.NoElementsInArrayException;
 
 public interface SearchService {
-    public int searchMaxValue(ArrayEntity array) throws NoElementsInArrayException;
-    public int searchMinValue(ArrayEntity array) throws  NoElementsInArrayException;
+    int searchMaxValue(ArrayEntity array) throws NoElementsInArrayException;
+    int searchMinValue(ArrayEntity array) throws  NoElementsInArrayException;
 }
--- src/main/java/by/silebin/arrays_task/service/exception/NoElementsInArrayException.java
+++ src/main/java/by/silebin/arrays_task/service/exception/NoElementsInArrayException.java
@@ -3,4 +3,17 @@ package by.silebin.arrays_task.service.exception;
 public class NoElementsInArrayException extends Exception {
     public NoElementsInArrayException() {
     }
+
+    public NoElementsInArrayException(String message) {
+        super(message);
+    }
+
+    public NoElementsInArrayException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NoElementsInArrayException(Throwable cause) {
+        super(cause);
+    }
+
 }
